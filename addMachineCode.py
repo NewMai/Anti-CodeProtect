44eac24612a9eebc7e926249f502324d48ed686d
NewMai <610098201@qq.com>
2019-10-08 21:11:22 +0800

    Update addMechineCode.py for using keystone on windows, but has something wrong with instruction:mov rax, qword ptr gs:[0x30]

--- addMachineCode.py
+++ addMachineCode.py
@@ -2,9 +2,15 @@
 # This file is used to run in Linux x64 system with pwntools installed.
 # And you need to change the base address variable (g_baseAddr) before using it.
 
-import pwn
-pwn.context.arch = "x86_64"  # For 64-bit architecture
-g_baseAddr = 0x400000
+# import pwn
+# pwn.context.arch = "x86_64"  # For 64-bit architecture
+# g_baseAddr = 0x400000
+
+# from keystone import *
+import keystone
+g_ks = keystone.Ks(keystone.KS_ARCH_X86, keystone.KS_MODE_64)  # Initialize engine in X86-64bit mode
+
+
 g_ErrLog = open("error.log", "a+")
 g_ErrLog.write("\r\n")
 
@@ -49,15 +55,15 @@ def dealwithBranch(curAddr, ins):
     offset2 = destAddr - curAddr
     if offset < 0x80 and offset > (-0x80): # short jmp,  2 bytes of machine code
         if offset > 0:
-            ins = "%s $+%d" % (arr[0], offset2) # $ represents the address of current instruction, not the rip
+            ins = "%s 0x%x" % (arr[0], offset2) # $ represents the address of current instruction, not the rip
         else: # offset < 0:
-            ins = "%s $+%d" % (arr[0], offset2) # e.g.  jz $+-0x11 ==> jz $-0x11
+            ins = "%s -0x%x" % (arr[0], 0-offset2) #
     else:   # long jmp,  2+4 bytes of machine code
         offset -= additionInsSize
         if offset > 0:
-            ins = "%s $+%d" % (arr[0], offset2)
+            ins = "%s 0x%x" % (arr[0], offset2)
         else: # offset < 0:
-            ins = "%s $+%d" % (arr[0], offset2)
+            ins = "%s -0x%x" % (arr[0], 0-offset2)
     return ins
 
 
@@ -75,35 +81,38 @@ def dealwithCall(curAddr, ins):
     offset = destAddr - curAddr - curInsSize
     offset2 = destAddr - curAddr
     if offset > 0:
-        ins = "%s $+%d" % (arr[0], offset2)
+        ins = "%s 0x%x" % (arr[0], offset2)
     else: # offset < 0
-        ins = "%s $+%d" % (arr[0], offset2)
+        ins = "%s -0x%x" % (arr[0], 0-offset2)
     return ins
 
 
 def getMachineCode(line):
-    global g_baseAddr
+    global g_ks
     arr = line.split("|")
     addr = arr[0]
     ins = arr[1].lower()
     ins = dealwithPtr(ins)
     ins = dealwithBranch(addr, ins)
     ins = dealwithCall(addr, ins)
+    mcode = 0
     try:
         # mcode = pwn.asm(ins, vma=g_baseAddr)
         if "nop" in ins and len(ins) > 3:
             # Unknown instruction, e.g. nop dword ptr [rax], eax
             # For the reason that we initialize all code into nop instruction at 
             #    the creation of a section
-            mcode = b'\90'
+            mcode = [0x90]
         else:
-            mcode = pwn.asm(ins)
-    except:
+            # mcode = pwn.asm(ins)
+            mcode, count = g_ks.asm(ins)
+    except Exception as e:
+        print str(e)
         raise RuntimeError("Unknow instruction for: %s" % ins)
     ret = ""
     for i in range(0, len(mcode)):
         x = mcode[i]
-        s = "%02x" % (ord(x))
+        s = "%02x" % (x)
         ret += s
     # Corresponding : bytes.fromhex('7370616d')
     return ret
@@ -130,12 +139,12 @@ def getMachineCodeOfIns(bblInst_file, fileNameOut):
 
                 if len(line) <= 16 or "|" not in line:    # Nothing to do
                     # print line
-                    fw.write(line+ "\r\n")
+                    fw.write(line+ "\n")
                 else:
                     mcode = getMachineCode(line)
                     line = "    %s|%s" % (line, mcode)
                     # print line
-                    fw.write(line+ "\r\n")
+                    fw.write(line+ "\n")
         pass
 
 
